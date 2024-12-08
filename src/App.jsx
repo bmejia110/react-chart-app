bc90cd09507697ba6568b4f754ae1eee2ec45f2c
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-12-08 13:11:26 -0500

    fetch chart data from JSON file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState, useEffect } from "react";
+import BarChart from "./components/BarChart.jsx";
+import LineChart from "./components/LineChart.jsx";
+import ScatterChart from "./components/ScatterChart.jsx";
+import BubbleChart from "./components/BubbleChart.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const[chartData, setChartData] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/financial_data.json");
+        if (!response.ok) {
+          throw new Error("failed to fetch the fata");
+        }
+        const data = await response.json();
+        setChartData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
 }
-
-export default App
